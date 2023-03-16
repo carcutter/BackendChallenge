4804f8c9b8eb835d07dc84fcfba91a24282c0237
Miro Jurisic <mirojurisic@hotmail.com>
2023-03-16 18:59:53 +0100

    feat(challenge): Added saving vehicles to json.

--- src/core/challenge_api.py
+++ src/core/challenge_api.py
@@ -1,11 +1,35 @@
-from flask import Blueprint, make_response
+from threading import Thread
+from flask import Blueprint, make_response, current_app as app, request
 from utils.api_decorators import ApiDecorators
+from werkzeug.utils import secure_filename
+
+from pathlib import Path
+import json, os
+
+# used low number for easy the testing, otherwise this numbers could be in 100 000s
+MAX_VEHICLE_COUNT = 10
 
 api = Blueprint("challenge_api", __name__)
+
+challenge_schema_filename = "schemas/vehicle-features.v1.schema.json"
+challenge_schema_abs_path = os.path.join(api.root_path, challenge_schema_filename)
+
+with open(challenge_schema_abs_path, "r") as f:
+    vehicle_feature_schema = json.load(f)
+
+
+def save_vehicle_to_json_file(filename, json_data):
+    # Overwrite the file
+    with open(filename, "w") as outfile:
+        json.dump(json_data, outfile, ensure_ascii=False)
 
 
 @api.route("/challenge", methods=["POST"])
 @ApiDecorators.require_customer_id
+@ApiDecorators.validate_vehicle_schema(vehicle_feature_schema)
+# Actually it would be better to limit the content length, this is just for demonstration purpose
+# By default, there is no limit, but huge file could crash the server
+@ApiDecorators.limit_vehicle_number(MAX_VEHICLE_COUNT)
 def vehicle_features_post(user_id: str):
     """
     Please see the README.md
@@ -15,9 +39,31 @@ def vehicle_features_post(user_id: str):
     This file should be stored to a folder named like the `user_id` and the filename should be the `id` with a ".json" extension.
 
     :param user_id: The id of the customer sending the request
-    :return: ???
+    :return: Dict of vehicle file(s) created/updated
+
     """
+    # I added this parent folder to make it clearer where are the stored folders
+    STORED_VEHICLES_FOLDER = os.path.join(api.root_path, "stored_vehicles_per_user_id", user_id)
+    if not os.path.exists(STORED_VEHICLES_FOLDER):
+        os.makedirs(STORED_VEHICLES_FOLDER)
 
-    # TODO Implement the challenge
+    # message contains files and status of file "Updated" or "Created"
+    return_message = {}
+    data = request.get_json()
+    app.logger.info("%d number of vehicles", len(data["vehicles"]))
 
-    return make_response("OK", 200)
+    for vehicle in data["vehicles"]:
+        # best practice not to trust client
+        secure_vehicle_id = secure_filename(f'{vehicle["id"]}.json')
+        vehicle_file_name = os.path.join(STORED_VEHICLES_FOLDER, secure_vehicle_id)
+        if os.path.exists(vehicle_file_name):
+            return_message[vehicle_file_name] = "Updated"
+        else:
+            return_message[vehicle_file_name] = "Created"
+        # if we were certain that the ids are unique, we could use multithreading for io task in parallel
+        save_vehicle_to_json_file(vehicle_file_name, vehicle)
+
+    app.logger.info("%d number of vehicles with unique id: ", len(return_message))
+
+    response = app.response_class(response=json.dumps(return_message), status=201, mimetype="application/json")
+    return response
--- src/utils/api_decorators.py
+++ src/utils/api_decorators.py
@@ -1,15 +1,77 @@
 from functools import wraps
 from uuid import uuid4
+from jsonschema import Draft4Validator
+from flask import request, jsonify
+from werkzeug.utils import secure_filename
+
+import json
 
 
 class ApiDecorators:
-
-    MOCK_CUSTOMER_ID = str(uuid4())
-
-    @staticmethod
     def require_customer_id(f):
-        @wraps(f)
-        def decorated_function(*args, **kwargs):
-            return f(ApiDecorators.MOCK_CUSTOMER_ID, *args, **kwargs)
+        """
+        Check does request contain user_id parameter
+        """
 
-        return decorated_function
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+            if "user_id" in request.args and request.args["user_id"] is not None:
+                # best practice not to trust client
+                # TODO: discuss should we secure the user_id
+                # !!! securing the user_id can change it !!!
+                secure_user_id = secure_filename(request.args["user_id"])
+                return f(secure_user_id, *args, **kwargs)
+            else:
+                response = jsonify(dict(success=False, message="invalid parameter, user_id is missing"))
+                # Not Acceptable
+                response.status_code = 406
+                return response
+
+        return wrapped
+
+    def validate_vehicle_schema(schema):
+        """
+        Check does json payload comply to vehicle schema
+        :param schema: Schema to validate payload against
+        """
+        validator = Draft4Validator(schema)
+
+        def wrapper(f):
+            @wraps(f)
+            def wrapped(*args, **kwargs):
+                input = request.get_json(force=True)
+                errors = [error.message for error in validator.iter_errors(input)]
+                # return errors response
+                if errors:
+                    response = jsonify(dict(success=False, message="Invalid payload", errors=errors))
+                    # Not Acceptable
+                    response.status_code = 406
+                    return response
+                # proceed to the function
+                else:
+                    return f(*args, **kwargs)
+
+            return wrapped
+
+        return wrapper
+
+    def limit_vehicle_number(max_vehicle_count):
+        """
+        Check does json payload contain more vehicles in the list than acceptable
+        :param max_vehicle_count: Maximum number of vehicles in vehicle list
+        """
+
+        def wrapper(f):
+            @wraps(f)
+            def wrapped(*args, **kwargs):
+                data = request.get_json()
+                if data is not None and len(data["vehicles"]) > max_vehicle_count:
+                    response = jsonify(dict(success=False, message=f"Invalid payload. Max number of vehicles is {max_vehicle_count}"))
+                    # Payload Too Large
+                    response.status_code = 413
+                    return response
+                return f(*args, **kwargs)
+
+            return wrapped
+
+        return wrapper
