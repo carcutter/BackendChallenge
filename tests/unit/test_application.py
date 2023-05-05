3162c35b431fe69bd6965865e4402eb5a0d19326
Behnam Haddadian <behnam.h1@gmail.com>
2023-05-05 13:02:02 +0300

    formated code

--- tests/unit/test_application.py
+++ tests/unit/test_application.py
@@ -19,14 +19,17 @@ def test_posting_invalid_data():
     with app.test_client() as client:
         response = client.post("/backend/challenge")
         assert response.status_code == 400
-        response = client.post("/backend/challenge", json={"vehicles": [{"id": "","features": [{"feature": "ENGINE","description": {"short": "5.2 Liter V8"}}]}]})
+        response = client.post(
+            "/backend/challenge",
+            json={"vehicles": [{"id": "", "features": [{"feature": "ENGINE", "description": {"short": "5.2 Liter V8"}}]}]},
+        )
         assert response.status_code == 400
 
+
 def test_posting_valid_data():
-	app = create_app()
-	with app.test_client() as client:
-		with open('json/vehicle-features.v1.example.json', 'r') as f:
-			json_data = json.load(f)
-		response = client.post("/backend/challenge", json=json_data)
-		assert response.status_code == 201
-		
+    app = create_app()
+    with app.test_client() as client:
+        with open("json/vehicle-features.v1.example.json", "r") as f:
+            json_data = json.load(f)
+        response = client.post("/backend/challenge", json=json_data)
+        assert response.status_code == 201
