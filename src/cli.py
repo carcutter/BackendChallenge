fa065de0327044dd605d18db2c5930fb7ee02ce7
Paul Albert <zone.buffer@gmail.com>
2023-05-20 14:49:24 +0300

    Updated cli.py - added parsing & loading for environments variables.

--- src/cli.py
+++ src/cli.py
@@ -1,6 +1,10 @@
 import click
+from dotenv import load_dotenv
 
 from applications.api_server import cli as api_server
+
+
+DOTENV_PATH = "./.env.cli"  # use path from root folder
 
 
 @click.group()
@@ -9,7 +13,10 @@ def cli(ctx):
     ctx.ensure_object(dict)
 
 
-cli.add_command(api_server)
-
 if __name__ == "__main__":
+    # load and parse the .env.cli file to environment variables
+    load_dotenv(dotenv_path=DOTENV_PATH)
+
+    # noinspection PyTypeChecker
+    cli.add_command(api_server)
     cli()
